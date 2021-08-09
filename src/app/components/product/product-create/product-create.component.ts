e3b73ab97f497ca567cfee089b6aabf520df370e
HonorioS <dasilvah77@gmail.com>
2021-08-09 13:13:47 +0100

    criando binding de atributo

--- src/app/components/product/product-create/product-create.component.ts
+++ src/app/components/product/product-create/product-create.component.ts
@@ -7,9 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductCreateComponent implements OnInit {
 
+  propLegal ="teste"
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  fazerAlgo(): void {
+    console.log("teste fazendo algo ... ")
   }
 
 }
